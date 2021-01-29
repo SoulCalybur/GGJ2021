8fe01b3121c5bb6dbdfdf5d41a26d230aa669962
JeremyB <jere2@hotmail.de>
2021-01-29 22:27:00 +0100

    ADD: ScriptableObjects

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "ScriptableObjects/ItemData", order = 2)]
+public class ItemData : ScriptableObject
+{
+    public string id;
+    public string name;
+    public string description;
+
+    public ItemData(string id, string name, string description) {
+
+    }
+
+
+}
