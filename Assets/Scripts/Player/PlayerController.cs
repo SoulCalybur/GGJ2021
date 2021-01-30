9a86c4998051bffb8f1c62f58c1bf5068584296d
JeremyB <jere2@hotmail.de>
2021-01-30 18:25:31 +0100

    Fixing Player input -  Rotate Player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,31 +6,43 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private InputActionReference movementInput;
-
+    private Vector2 moveInput = new Vector2();
     private CharacterController controller;
     private Vector3 velocity;
     [SerializeField]
     private float moveSpeed = 5f;
+    [SerializeField]
+    private float rotationSpeed = 5f;
     private float gravity = -9.81f;
 
+    private Transform cameraTransform;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void PollMovementInput(InputAction.CallbackContext context) {
+        Debug.Log(context);
+        moveInput = context.ReadValue<Vector2>();
     }
-
+    private void OnEnable() {
+    }
+    private void Start() {
+        controller = gameObject.GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
+    }
     // Update is called once per frame
     void Update() {
         velocity.y = 0;
 
-        Vector2 moveInput = movementInput.action.ReadValue<Vector2>();
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+        move.y = 0;
         controller.Move(move * Time.deltaTime * moveSpeed);
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+        if(moveInput != Vector2.zero) {
+            float targetAngle = Mathf.Atan2(moveInput.x, moveInput.y) * Mathf.Rad2Deg +cameraTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
     }
 }
