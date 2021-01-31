a94411fecf7ca7f475b1264bf9de63a1dda133db
JeremyB <jere2@hotmail.de>
2021-01-31 14:12:25 +0100

    find closest item

--- Assets/Scripts/Player/DiggingAction.cs
+++ Assets/Scripts/Player/DiggingAction.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class DiggingAction : MonoBehaviour
 {
+
+    public BuriedItem[] buriedItems;
+    private BuriedItem foundItem;
+
     [SerializeField]
     private GameObject uiBar;
     private Slider progressBar;
@@ -18,6 +22,11 @@ public class DiggingAction : MonoBehaviour
     public IEnumerator startDigging() {
         digHole = Instantiate(digPrefab, this.transform.position + this.transform.forward * 1.4f, Quaternion.identity);
         progressBar.value = 0.3f;
+
+        foundItem = findClosestItem();
+        if(foundItem != null) {
+            foundItem.transform.position = digHole.transform.position + new Vector3(0, -1 + progressBar.value, 0);
+        }
         yield break;
     }
 
@@ -25,6 +34,22 @@ public class DiggingAction : MonoBehaviour
         increaseValue(0.15f);
         Debug.Log("Digging deeper!! to:" + progressBar.value);
     }
+
+    private BuriedItem findClosestItem() {
+        BuriedItem closest = null;
+        for (int i = 0; i < buriedItems.Length; i++) {
+            float minDistItem = buriedItems[i].GetComponent<AudioSource>().minDistance;
+            Vector3 itemPos = buriedItems[i].transform.position;
+            Vector3 playerPos = this.transform.position;
+            float distToItem = Vector3.Distance(playerPos, itemPos);
+            if(distToItem <= minDistItem) {
+                closest = buriedItems[i];
+                break;
+            }
+        }
+        return closest;
+    }
+
     void setValue(float val) {
         // val should be [0,1]
         val = Mathf.Clamp(val, 0, 1);
@@ -38,22 +63,24 @@ public class DiggingAction : MonoBehaviour
     void increaseValue(float amount) {
         progressBar.value += amount;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        progressBar = uiBar.GetComponent<Slider>();
-        controller = gameObject.GetComponent<PlayerController>();
-    }
+    
 
-    // Update is called once per frame
     void Update()
     {   
         if(controller.isDigging) {
             progressBar.value -= Time.deltaTime * 0.5f;
             digHole.transform.localScale = new Vector3(1f + progressBar.value, 1, 1f + progressBar.value);
+            if(foundItem != null) {
+                foundItem.transform.position = digHole.transform.position + new Vector3(0, -1 + progressBar.value, 0);
+            }
             if(progressBar.value <= 0.00005f) {
                 controller.isDigging = false;
             }
         }
+    }
+    void Start() {
+        progressBar = uiBar.GetComponent<Slider>();
+        controller = gameObject.GetComponent<PlayerController>();
+        buriedItems = Object.FindObjectsOfType<BuriedItem>();
     }
 }
